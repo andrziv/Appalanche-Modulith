779e4c768729565fa7f74d5c06419bd325f90d36
andrziv <soveryhappyface@gmail.com>
2025-11-12 19:27:39 -0500

    Initial commit with spring requirements.

---
+++ src/main/java/com/jobhunt/backend/JobHunt_Modulith/JobHuntModulithApplication.java
@@ -0,0 +1,13 @@
+package com.jobhunt.backend.JobHunt_Modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobHuntModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobHuntModulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobhunt/backend/JobHunt_Modulith/JobHuntModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobhunt.backend.JobHunt_Modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHuntModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
