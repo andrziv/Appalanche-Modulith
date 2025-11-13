43fcabf2938840ec639ac7d83e98df0fe02516c9
andrziv <soveryhappyface@gmail.com>
2025-11-13 17:56:46 -0500

    testing jenkins container again

--- src/main/java/com/jobhunt/backend/JobHunt_Modulith/JobHuntModulithApplication.java
+++ src/main/java/com/jobhunt/backend/JobHunt_Modulith/JobHuntModulithApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobHuntModulithApplication {
-// undo
+
 	public static void main(String[] args) {
 		SpringApplication.run(JobHuntModulithApplication.class, args);
 	}
