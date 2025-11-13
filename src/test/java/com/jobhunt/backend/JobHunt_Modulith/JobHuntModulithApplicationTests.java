a3c1b78949d1f30be291116a323933e1a9963fc4
andrziv <soveryhappyface@gmail.com>
2025-11-13 17:26:09 -0500

    testing a forceful version of the previous commit

--- src/test/java/com/jobhunt/backend/JobHunt_Modulith/JobHuntModulithApplicationTests.java
+++ src/test/java/com/jobhunt/backend/JobHunt_Modulith/JobHuntModulithApplicationTests.java
@@ -3,7 +3,13 @@ package com.jobhunt.backend.JobHunt_Modulith;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:db;DB_CLOSE_DELAY=-1",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class JobHuntModulithApplicationTests {
 
 	@Test
