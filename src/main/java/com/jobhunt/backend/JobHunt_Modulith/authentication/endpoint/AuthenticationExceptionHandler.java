2f992dd38971a2baa46574d29525baa533ee0b1d
andrziv <soveryhappyface@gmail.com>
2025-11-25 19:35:09 -0500

    Undoing previous change because it ultimately doesn't seem impactful. - Removing login request validators because the validator should be within the user's head (i.e. their actual username/password).

--- src/main/java/com/jobhunt/backend/JobHunt_Modulith/authentication/endpoint/AuthenticationExceptionHandler.java
+++ src/main/java/com/jobhunt/backend/JobHunt_Modulith/authentication/endpoint/AuthenticationExceptionHandler.java
@@ -26,7 +26,7 @@ public class AuthenticationExceptionHandler {
     public ResponseEntity<Map<String, String>> handleBadCredentials(BadCredentialsException ex) {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of("error", "Login Failed", "message", "Invalid username or password"));
+                .body(Map.of("error", "Login Failed", "message", "Invalid email or password"));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
