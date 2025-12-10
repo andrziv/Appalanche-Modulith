69783d90cb3763c6c4b0f0853e11b4004077dbe7
andrziv <soveryhappyface@gmail.com>
2025-12-09 21:02:18 -0500

    Updating the profile controller to not leak the database ID of the users on getting the profile.

---
+++ src/main/java/com/appalanche/backend/profiles/business/request_response/GetAccountProfileResponse.java
@@ -0,0 +1,25 @@
+package com.appalanche.backend.profiles.business.request_response;
+
+import com.appalanche.backend.profiles.persistence.AccountProfile;
+
+import java.util.UUID;
+
+public record GetAccountProfileResponse(
+        UUID accountId,
+        String firstName,
+        String lastName,
+
+        String linkedInProfile,
+        String gitHubProfile,
+        String portfolioSite) {
+
+    public static GetAccountProfileResponse from(AccountProfile profile) {
+        return new GetAccountProfileResponse(
+                profile.getAccountId(),
+                profile.getFirstName(),
+                profile.getLastName(),
+                profile.getLinkedInProfile(),
+                profile.getGitHubProfile(),
+                profile.getPortfolioSite());
+    }
+}
--- src/main/java/com/appalanche/backend/profiles/endpoint/AccountProfileController.java
+++ src/main/java/com/appalanche/backend/profiles/endpoint/AccountProfileController.java
@@ -1,8 +1,8 @@
 package com.appalanche.backend.profiles.endpoint;
 
 import com.appalanche.backend.profiles.business.AccountProfileService;
+import com.appalanche.backend.profiles.business.request_response.GetAccountProfileResponse;
 import com.appalanche.backend.profiles.business.request_response.ModifyAccountProfileRequest;
-import com.appalanche.backend.profiles.persistence.AccountProfile;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,8 +19,9 @@ public class AccountProfileController {
 
     @GetMapping
     @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<AccountProfile> getProfile() {
-        return ResponseEntity.ok(profileService.getProfile());
+    public ResponseEntity<GetAccountProfileResponse> getProfile() {
+        var profile = profileService.getProfile();
+        return ResponseEntity.ok(GetAccountProfileResponse.from(profile));
     }
 
     @PatchMapping
